2175646d00ada29d50e3ff0c99153643834ffd8a
zaimov <denislav.zaimov@gmail.com>
2022-12-12 17:54:55 -0500

    added cart context, component and dropdown. Added products card and shop component.

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -6,9 +6,13 @@ import {ReactComponent as CrownLogo} from '../../assets/crown.svg';
 import './navigation.styles.scss';
 import {UserContext} from '../../contexts/user.context';
 import {signOutUser} from '../../utils/firebase/firebase.utils';
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
+import {CartContext} from '../../contexts/cart.context';
 
 const Navigation = () => {
   const {currentUser} = useContext(UserContext);
+  const {isCartOpen} = useContext(CartContext);
 
   return (
     <Fragment>
@@ -23,7 +27,9 @@ const Navigation = () => {
           ) : (
             <Link className='nav-link' to='/auth'>Sign in</Link>
           )}
+          <CartIcon />
         </div>
+        {isCartOpen && <CartDropdown />}
       </div>
       <Outlet/>
     </Fragment>
