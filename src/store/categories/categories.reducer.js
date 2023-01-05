8ba8caa22bec5a6c5d95685b2f2ad4a3247bee8b
zaimov <denislav.zaimov@gmail.com>
2023-01-05 11:38:04 -0500

    added categories success case in reducer

--- src/store/categories/categories.reducer.js
+++ src/store/categories/categories.reducer.js
@@ -12,8 +12,10 @@ export const categoriesReducer = (state = CATEGORIES_INITIAL_STATE, action = {})
 
   switch (type) {
     case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START:
-      return {...state, isLoading: true};
+      return { ...state, isLoading: true };
     case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS:
+      return { ...state, categories: payload, isLoading: false };
+    case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED:
       return { ...state, error: payload, isLoading: false };
     default:
       return state;
